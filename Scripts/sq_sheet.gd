b78735a2707c796d5a4eeebdac927eb4511a86f9
Patrick Ly <patrickly7@yahoo.com>
2024-10-01 14:49:00 -0400

    Added New Ship Station sprites, started sq sheet gen work

--- Scripts/sq_sheet.gd
+++ Scripts/sq_sheet.gd
@@ -1,5 +1,58 @@
 extends Control
 
+var rng = RandomNumberGenerator.new()
+
+var validNumbers = '0123456789'
+var isComplete = false
+
+func _ready():
+	rng.randomize()
+	generateSQ()
+
+func _process(delta):
+	if $SQNumber.text != Global.currentSQ && isComplete:
+		isComplete = false
+		generateSQ()
+		
+	if isComplete:
+		$CompleteStamp.show()
+	else:
+		$CompleteStamp.hide()
+
+func generateSQNumber():
+	var sqNumber = "SQ-"
+	for i in range(6):
+		sqNumber += validNumbers[randi() % 6]
+	return sqNumber
+
+func generateSQ():
+	Global.cardsToPull = []
+	Global.cardsPulled = []
+	
+	# Create SQ Number
+	var sqNumber = generateSQNumber()
+	Global.currentSQ = sqNumber
+	$SQNumber.text = sqNumber
+	
+	# Add Products
+	var numSkus = rng.randi_range(2, 4)
+	print("SKUs on SQ: " + str(numSkus))
+	
+	const possibleCardsLen = len(Global.possibleCards)
+	var totalQuantity = 0
+	
+	for i in range(numSkus):
+		var quantity = rng.randi_range(1, 3)
+		Global.cardsToPull.append({
+			Global.possibleCards[randi() % possibleCardsLen]: quantity
+		})
+		totalQuantity += quantity
+	print(Global.cardsToPull)
+	
+	# Calculate Weight
+	var weight = totalQuantity * 0.2
+	$Weight.text = str(weight) + " oz"
+
 func _on_hide_button_pressed():
 	$ShowButton.show()
 	$HideButton.hide()
---
+++ global.gd
@@ -0,0 +1,33 @@
+# Global Variables
+extends Node
+
+const possibleCards = [
+	"Bat",
+	"Candy Bat",
+	"Candy Ghost",
+	"Candy Pumpkin",
+	"Candy Tree",
+	"Ghost",
+	"Pumpkin",
+	"Snowflake Bat",
+	"Snowflake Ghost",
+	"Snowflake Pumpkin",
+	"Snowflake Tree",
+	"Tree",
+	"Candy Candy Corn",
+	"Candy Corn",
+	"Candy Scarecrow",
+	"Candy Spider",
+	"Candy Witch",
+	"Scarecrow",
+	"Snowflake Candy Corn",
+	"Snowflake Scarecrow",
+	"Snowflake Spider",
+	"Snowflake Witch",
+	"Spider",
+	"Witch"
+]
+
+var cardsToPull = []
+var cardsPulled = []
+var currentSQ = ""
