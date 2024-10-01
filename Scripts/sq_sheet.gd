27853bf89560baceaa1278a6238e78a153e1a454
Patrick Ly <patrickly7@yahoo.com>
2024-10-01 15:14:24 -0400

    SQ Sheet now shows the cards you need to pull

--- Scripts/sq_sheet.gd
+++ Scripts/sq_sheet.gd
@@ -34,23 +34,55 @@ func generateSQ():
 	Global.currentSQ = sqNumber
 	$SQNumber.text = sqNumber
 	
-	# Add Products
+	# Calculate How Many SKUs to Have
 	var numSkus = rng.randi_range(2, 4)
 	print("SKUs on SQ: " + str(numSkus))
 	
 	const possibleCardsLen = len(Global.possibleCards)
 	var totalQuantity = 0
 	
+	if numSkus == 2:
+		$SQItems/SQItem3.hide()
+		$SQItems/SQItem4.hide()
+	elif numSkus == 3:
+		$SQItems/SQItem3.show()
+		$SQItems/SQItem4.hide()
+	else:
+		$SQItems/SQItem3.show()
+		$SQItems/SQItem4.show()
+	
+	# Fetch the SKUs and Generate Quantities for them
 	for i in range(numSkus):
+		var cardName = Global.possibleCards[randi() % possibleCardsLen]
 		var quantity = rng.randi_range(1, 3)
+		
+		if i == 0:
+			$SQItems/SQItem1/CardName.text = cardName
+			$SQItems/SQItem1/Quantity.text = str(quantity)
+			$SQItems/SQItem1/SetIcon.texture = load(Global.cardToSetIconDict[cardName])
+		elif i == 1:
+			$SQItems/SQItem2/CardName.text = cardName
+			$SQItems/SQItem2/Quantity.text = str(quantity)
+			$SQItems/SQItem2/SetIcon.texture = load(Global.cardToSetIconDict[cardName])
+		elif i == 2:
+			$SQItems/SQItem3/CardName.text = cardName
+			$SQItems/SQItem3/Quantity.text = str(quantity)
+			$SQItems/SQItem3/SetIcon.texture = load(Global.cardToSetIconDict[cardName])
+		elif i == 3:
+			$SQItems/SQItem4/CardName.text = cardName
+			$SQItems/SQItem4/Quantity.text = str(quantity)
+			$SQItems/SQItem4/SetIcon.texture = load(Global.cardToSetIconDict[cardName])
+		
 		Global.cardsToPull.append({
-			Global.possibleCards[randi() % possibleCardsLen]: quantity
+			cardName: quantity
 		})
+		
 		totalQuantity += quantity
 	print(Global.cardsToPull)
 	
 	# Calculate Weight
 	var weight = totalQuantity * 0.2
+	Global.currentSQWeight = weight
 	$Weight.text = str(weight) + " oz"
 
 func _on_hide_button_pressed():
--- global.gd
+++ global.gd
@@ -28,6 +28,43 @@ const possibleCards = [
 	"Witch"
 ]
 
+const cardToSetIconDict = {
+	# Bow
+	"Bat": "res://Assets/Art/Icons/Set_Bow.png",
+	"Candy Bat": "res://Assets/Art/Icons/Set_Bow.png",
+	"Candy Ghost": "res://Assets/Art/Icons/Set_Bow.png",
+	"Candy Pumpkin": "res://Assets/Art/Icons/Set_Bow.png",
+	"Candy Tree": "res://Assets/Art/Icons/Set_Bow.png",
+	"Ghost": "res://Assets/Art/Icons/Set_Bow.png",
+	"Pumpkin": "res://Assets/Art/Icons/Set_Bow.png",
+	"Snowflake Bat": "res://Assets/Art/Icons/Set_Bow.png",
+	"Snowflake Ghost": "res://Assets/Art/Icons/Set_Bow.png",
+	"Snowflake Pumpkin": "res://Assets/Art/Icons/Set_Bow.png",
+	"Snowflake Tree": "res://Assets/Art/Icons/Set_Bow.png",
+	"Tree": "res://Assets/Art/Icons/Set_Bow.png",
+	
+	# Shield
+	"Candy Candy Corn": "res://Assets/Art/Icons/Set_Shield.png",
+	"Candy Corn": "res://Assets/Art/Icons/Set_Shield.png",
+	"Candy Scarecrow": "res://Assets/Art/Icons/Set_Shield.png",
+	"Candy Spider": "res://Assets/Art/Icons/Set_Shield.png",
+	"Candy Witch": "res://Assets/Art/Icons/Set_Shield.png",
+	"Scarecrow": "res://Assets/Art/Icons/Set_Shield.png",
+	"Snowflake Candy Corn": "res://Assets/Art/Icons/Set_Shield.png",
+	"Snowflake Scarecrow": "res://Assets/Art/Icons/Set_Shield.png",
+	"Snowflake Spider": "res://Assets/Art/Icons/Set_Shield.png",
+	"Snowflake Witch": "res://Assets/Art/Icons/Set_Shield.png",
+	"Spider": "res://Assets/Art/Icons/Set_Shield.png",
+	"Witch": "res://Assets/Art/Icons/Set_Shield.png"
+}
+
 var cardsToPull = []
 var cardsPulled = []
 var currentSQ = ""
+var currentSQWeight = 0
+
+# Used for Scoring
+var sqsCompleted = 0
+var wrongShippingUsed = 0
+var wrongCardsPulled = 0
+var correctCardsPulled = 0
