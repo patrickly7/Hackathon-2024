0faf9b7aad5cf8f442da16507a3cf1e5b4bbeb32
Patrick Ly <patrickly7@yahoo.com>
2024-10-02 10:32:05 -0400

    Added Game Logo and Stamp

--- Scripts/results_screen.gd
+++ Scripts/results_screen.gd
@@ -1,10 +1,25 @@
 extends Control
 
 func _ready():
+	await get_tree().create_timer(1).timeout
+	$CorrectCardsPulled.show()
 	$CorrectCardsPulled.text = "Correct Cards Pulled: " + str(Global.correctCardsPulled)
+	
+	await get_tree().create_timer(1).timeout
+	$IncorrectCardsPulled.show()
 	$IncorrectCardsPulled.text = "Inorrect Cards Pulled: " + str(Global.wrongCardsPulled)
+	
+	await get_tree().create_timer(1).timeout
+	$SQsCompleted.show()
 	$SQsCompleted.text = "SQs Completed: " + str(Global.sqsCompleted)
+	
+	await get_tree().create_timer(1).timeout
+	$TimesWrongShippingUsed.show()
 	$TimesWrongShippingUsed.text = "Times Wrong Shipped was Used: " + str(Global.wrongShippingUsed)
+	
+	await get_tree().create_timer(1).timeout
+	$PlayAgainButton.show()
+	$QuitButton.show()
 
 func _on_quit_button_pressed():
 	$ButtonClickSound.play()
