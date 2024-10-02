6aa6ac663aa26d8e4f663e322f0227b22d2ba241
Rose Cronk <85558652+RoseMarieW@users.noreply.github.com>
2024-10-01 22:51:18 -0400

    Help basic page

--- Scripts/HelpMenu.gd
+++ Scripts/HelpMenu.gd
@@ -1,5 +1,38 @@
 extends Control
 
+var page = 0
+
 func _on_close_button_pressed():
 	get_tree().paused = false
 	hide()
+
+func _on_left_arrow_pressed():
+	page -= 1
+	change_page()
+
+
+func _on_right_arrow_pressed():
+	page += 1
+	change_page()
+
+func change_page():
+	if page == 0:
+		$IntroPage.show()
+		$ControlsPage.hide()
+		$SetsPage.hide()
+		$PackageTypesPage.hide()
+	elif page == 1:
+		$IntroPage.hide()
+		$ControlsPage.show()
+		$SetsPage.hide()
+		$PackageTypesPage.hide()
+	elif page == 2:
+		$IntroPage.hide()
+		$ControlsPage.hide()
+		$SetsPage.show()
+		$PackageTypesPage.hide()
+	elif page == 3:
+		$IntroPage.hide()
+		$ControlsPage.hide()
+		$SetsPage.hide()
+		$PackageTypesPage.show()
