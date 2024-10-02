7e7fe07183bce099977a13fa2f611c9d9b9a6a1d
Rose Cronk <85558652+RoseMarieW@users.noreply.github.com>
2024-10-02 07:16:04 -0400

    placement instead of anchor, page limits

--- Scripts/HelpMenu.gd
+++ Scripts/HelpMenu.gd
@@ -7,12 +7,14 @@ func _on_close_button_pressed():
 	hide()
 
 func _on_left_arrow_pressed():
-	page -= 1
+	if page > 0:
+		page -= 1
 	change_page()
 
 
 func _on_right_arrow_pressed():
-	page += 1
+	if page < 3:
+		page += 1
 	change_page()
 
 func change_page():
